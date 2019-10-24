e03a9c43c1828807a1236dc8a3ddb940db55a94c
nweiji2 <nweiji2@cn.eit>
2019-10-24 14:48:24 +0800

    fixed css problem on apache

--- public_html/resources/js/events.js
+++ public_html/resources/js/events.js
@@ -1,15 +1,15 @@
 var events_log = {};
 
-$(document).ready(function () {
+$(document).ready(function() {
     populateAPI();
-    $("#nonce").val(makeid(12));
+    //$("#nonce").val(makeid(12));
     //Generate a nonce string to validate returned http response
-    $("#js_generate_nonce").on("click", (event)=>{
+    $("#js_generate_nonce").on("click", (event) => {
         event.preventDefault();
         $("#nonce").val(makeid(12));
     });
-    
-    $("#js_post").on("click", (event)=>{
+
+    $("#js_post").on("click", (event) => {
         event.preventDefault();
         var targetURL = $("#urlInput").val();
         updatePostEvent(targetURL);
@@ -22,67 +22,92 @@ $(document).ready(function () {
 function updatePostEvent(targetURL) {
 
     $("#js_post").html('<div class="spinner-border text-light"></div>');
-    var post_data =  {
+    var post_data = {
         "origin": window.location.protocol + "//" + window.location.host,
-        "destination":targetURL,
-        "nonce":"" + $("#nonce").val()
+        "destination": targetURL,
+        "nonce": "" + $("#nonce").val()
     };
 
     $.ajax({
-        url: targetURL,
+        url: targetURL + "/api/post",
         type: "POST",
         contentType: "application/json",
         data: JSON.stringify(post_data),
-        success: function(result){
+        success: function(result) {
             console.log(result);
             updateHttpEvent("success", targetURL, result);
         },
-        error:function(error){
+        error: function(error) {
             console.log(error);
             updateHttpEvent("failure", targetURL, error);
         }
     })
 }
 
-function updateHttpEvent(action,targetURL, data) {
+function updateHttpEvent(action, targetURL, data) {
     var today = new Date();
     var time = pad(today.getHours()) + ":" + pad(today.getMinutes()) + ":" + pad(today.getSeconds());
-    $("#js_post").html('Post');
+    $("#js_post").html('Send');
     var message;
     var statusClass;
-    if(action == "success"){
-        message = '<span class="message-success">[SUCCESS]</span>' + " <b> Origin:</b> " + window.location.href + " <b> Destination:</b> " + targetURL + " <b>Response:</b> " + data + '<span class="float-right">'+" <b>[</b>" + time + "<b>]</b> </span>";
-        statusClass = "event-success";
-    }else if(action == "received"){
-        message = '<span class="message-received">[RECEIVED]</span>' + " <b> Origin:</b> " + targetURL + " <b>Message:</b> " + data + '<span class="float-right">' + " <b>[</b>" + time + "<b>]</b> </span>";
-        statusClass = "event-received";
+    if (action == "success") {
+        message = `<div class="card bg-success text-white mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Message Success</h5>
+                            <p class="card-text">` + data + `</p>
+                            <p class="card-text">
+                                <span><b>Receiver:</b> ` + targetURL + `</span>
+                                <span class="float-right"><b>` + time + `</b></span>
+                            </p>
+                        </div>
+                    </div>`;
+
+    } else if (action == "received") {
+        message = `<div class="card event-received text-white mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Message Received</h5>
+                            <p class="card-text">` + data + `</p>
+                            <p class="card-text">
+                                <span><b>Sender:</b> ` + targetURL + `</span>
+                                <span class="float-right"><b>` + time + `</b></span>
+                            </p>
+                        </div>
+                    </div>`;
+
+    } else {
+        message = `<div class="card bg-danger text-white mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Message Failure</h5>
+                            <p class="card-text">Unable to deliver message, destination host is unreachable.</p>
+                            <p class="card-text">
+                                <span><b>Receiver:</b> ` + targetURL + `</span>
+                                <span class="float-right"><b>` + time + `</b></span>
+                            </p>
+                        </div>
+                    </div>`;
     }
-    else{
-        message = '<span class="message-failure">[FAILURE]</span>' + " <b> Origin:</b> " + window.location.href + " <b> Destination: </b> " + targetURL +'<span class="float-right">' + " <b>[</b>" + time + "<b>]</b> </span>";
-        statusClass = "event-failure";
-    }
-    $("#js_events_parent").prepend('<p class="event-row '+statusClass+'">'+ message +'</p>');
+    $("#js_events_parent").prepend(message);
 }
 
-function checkReceivedMessages(){
+function checkReceivedMessages() {
     $.ajax({
         url: "/api/retrieve",
         type: "GET",
-        dataType:"json",
+        dataType: "json",
         async: true,
-        success: function (result) {
-            if(result["valid"]){
+        success: function(result) {
+            if (result["valid"]) {
                 console.log(result);
                 showNotification(result);
             }
         },
-        error: function (error) {
+        error: function(error) {
             //console.log("no_messages", error);
         }
     })
 }
 
-function showNotification(data){
+function showNotification(data) {
     $("#js_post_origin").html(data["origin"]);
     $("#js_post_message").html(data["nonce"]);
     $('#js_post_toast').toast('show');
@@ -101,8 +126,8 @@ function makeid(length) {
 
 function pad(n) { return n < 10 ? '0' + n : n }
 
-function populateAPI(){
-    var apiString = window.location.protocol + "//" + window.location.host + "/api/post";
+function populateAPI() {
+    var apiString = window.location.protocol + "//" + window.location.host;
     $("#apiURL").html(apiString);
-    $("#urlInput").val(apiString);
+    //$("#urlInput").val(apiString);
 }
